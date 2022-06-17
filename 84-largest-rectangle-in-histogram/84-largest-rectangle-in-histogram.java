6658e5bbe26e76889c96a00551d0a52e514fda70
Roshan-Pawar <90392961+Roshan-Pawar@users.noreply.github.com>
2022-06-17 19:19:57 +0530

    Time: 355 ms (12.61%), Space: 95.5 MB (28.48%) - LeetHub

--- 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.java
+++ 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.java
@@ -1,34 +1,39 @@
 class Solution {
     public int largestRectangleArea(int[] heights) {
         int n = heights.length;
-        int maxArea = 0;
-        
+        Stack<Integer> s = new Stack<>();
         int[] left = new int[n];
         int[] right = new int[n];
         
-        left[0] = -1;
-        right[n - 1] = n;
-        
+        // Loop to find left small elements index
         for(int i = 0; i < n; i++){
-            int prev = i - 1;
-            while(prev >= 0 && heights[prev] >= heights[i]){
-                prev = left[prev];
+            while(!s.empty() && heights[s.peek()] >= heights[i]){
+                s.pop();
             }
-            left[i] = prev;   
+            if(s.empty()) left[i] = 0;
+            else left[i] = s.peek() + 1;
+            s.push(i);
         }
         
-        for(int i = n - 2; i >= 0; i--){
-            int prev = i + 1;
-            while(prev < n && heights[prev] >= heights[i]){
-                prev = right[prev];
+        s.clear();
+        
+        // Loop to find Right small elements index
+        for(int i = n - 1; i >= 0; i--){
+            while(!s.empty() && heights[s.peek()] >= heights[i]){
+                s.pop();
             }
-            right[i] = prev;
+            if(s.empty()) right[i] = n - 1;
+            else right[i] = s.peek() - 1;
+            s.push(i);
         }
         
+        int maxArea = 0;
+        // Check for maxArea using array of left and right small of "i"
         for(int i = 0; i < n; i++){
-            int width = right[i] - left[i] - 1;
-            maxArea = Math.max(maxArea, heights[i] * width);
+            maxArea = Math.max(maxArea, heights[i] * (right[i] - left[i] + 1));
         }
         return maxArea;
+        
+        // Time and Space O(N)
     }
 }
