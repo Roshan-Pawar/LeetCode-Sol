c11525d97761b9be2699758058177805d42c23a5
Roshan-Pawar <90392961+Roshan-Pawar@users.noreply.github.com>
2022-06-17 19:31:23 +0530

    Time: 187 ms (49.00%), Space: 86.9 MB (62.46%) - LeetHub

--- 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.java
+++ 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.java
@@ -1,39 +1,22 @@
 class Solution {
     public int largestRectangleArea(int[] heights) {
-        int n = heights.length;
         Stack<Integer> s = new Stack<>();
-        int[] left = new int[n];
-        int[] right = new int[n];
+        int maxArea = 0, n = heights.length;
         
-        // Loop to find left small elements index
-        for(int i = 0; i < n; i++){
-            while(!s.empty() && heights[s.peek()] >= heights[i]){
+        for(int i = 0; i <= n; i++){
+            while(!s.empty() && (i == n || heights[s.peek()] >= heights[i])){
+                int height = heights[s.peek()];
                 s.pop();
+                int width;
+                if(s.empty()) width = i;
+                else width = i - s.peek() - 1;
+                maxArea = Math.max(maxArea, width * height);
             }
-            if(s.empty()) left[i] = 0;
-            else left[i] = s.peek() + 1;
             s.push(i);
-        }
-        
-        s.clear();
-        
-        // Loop to find Right small elements index
-        for(int i = n - 1; i >= 0; i--){
-            while(!s.empty() && heights[s.peek()] >= heights[i]){
-                s.pop();
-            }
-            if(s.empty()) right[i] = n - 1;
-            else right[i] = s.peek() - 1;
-            s.push(i);
-        }
-        
-        int maxArea = 0;
-        // Check for maxArea using array of left and right small of "i"
-        for(int i = 0; i < n; i++){
-            maxArea = Math.max(maxArea, heights[i] * (right[i] - left[i] + 1));
         }
         return maxArea;
-        
-        // Time and Space O(N)
     }
+    // Time = O(N), Space = O(N)
+    // for previous submission of finding left small and right small,  Space = O(3N) because of 2Arrays & 1Stack
+    // this approach is compilicated only solve if wants to optimize the previous one.
 }
