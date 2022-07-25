ef1ec59625ef23955f1c8f88c02925b73a2b40e9
Roshan-Pawar <90392961+Roshan-Pawar@users.noreply.github.com>
2022-07-25 18:22:38 +0530

    Time: 4 ms (46.24%), Space: 43.4 MB (79.13%) - LeetHub

--- 2089-find-target-indices-after-sorting-array/2089-find-target-indices-after-sorting-array.java
+++ 2089-find-target-indices-after-sorting-array/2089-find-target-indices-after-sorting-array.java
@@ -1,19 +1,34 @@
 class Solution {
     public List<Integer> targetIndices(int[] nums, int target) {
+        int start = binarySearch(nums, target, true);
+        int end = binarySearch(nums, target, false);
         List<Integer> list = new ArrayList<>();
-        int targetFreq = 0, lessThanTarget = 0;
         
-        for(int num : nums){
-            if(num == target)
-                targetFreq++;
-            if(num < target)
-                lessThanTarget++;
-        }
-        
-        for(int i = lessThanTarget; i < (lessThanTarget + targetFreq); i++){
+        for(int i = start; i <= end; i++){
+            if(i != -1)
             list.add(i);
         }
         return list;
     }
-    // TC : O(n) SC : O(1)
+    
+    public int binarySearch(int[] nums, int target, boolean isFirst){
+        int start = 0, end = nums.length - 1, ans = -1;
+        Arrays.sort(nums);
+        
+        while(start <= end){
+            int mid = start + (end - start) / 2;
+            if(target < nums[mid])
+                end = mid - 1;
+            else if (target > nums[mid])
+                start = mid + 1;
+            else{
+                ans = mid;
+                if(isFirst)
+                    end = mid - 1;
+                else
+                    start = mid + 1;
+            }
+        }
+        return ans;
+    } 
 }
