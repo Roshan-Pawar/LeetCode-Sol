a5bf1c7906a187e6a0d9182e5c63dc11f9d5b954
Roshan-Pawar <90392961+Roshan-Pawar@users.noreply.github.com>
2022-07-28 17:17:35 +0530

    Time: 34 ms (10.24%), Space: 46.6 MB (17.67%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.java
+++ 242-valid-anagram/242-valid-anagram.java
@@ -2,16 +2,19 @@ class Solution {
     public boolean isAnagram(String s, String t) {
         if(s.length() != t.length()) return false;
         
-        char arrs[] = s.toCharArray();
-        char arrt[] = t.toCharArray();
-        // Convert the String into array then sort it and compare every element
-        Arrays.sort(arrs);
-        Arrays.sort(arrt);
-        
-        for(int i = 0; i < arrs.length; i++){
-            if(arrs[i] != arrt[i]) return false;
+        Map<Character, Integer> map = new HashMap<>();
+        // Put all the elements of String S into map & increment value if double char found 
+        for(int i = 0; i < s.length(); i++){ 
+            map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0) + 1);
         }
-        return true;
-    } 
-    // TC : O(n log n)  SC : O(n)
+        
+        for(int i = 0; i < s.length(); i++){
+            if(!map.containsKey(t.charAt(i))) return false;
+            // if value of that char is more than 1 then reduse it by 1
+            if(map.get(t.charAt(i)) > 1) map.put(t.charAt(i), map.get(t.charAt(i)) - 1); 
+            else map.remove(t.charAt(i));
+        }
+        return map.size() == 0;
+    }
+    // TC & SC : O(n)
 }
