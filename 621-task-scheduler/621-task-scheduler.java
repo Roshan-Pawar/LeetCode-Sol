a7e124fa5f4ca07de440294da93461b6aeb28528
Roshan-Pawar <90392961+Roshan-Pawar@users.noreply.github.com>
2022-08-06 15:17:04 +0530

    Time: 4 ms (76.36%), Space: 58.7 MB (81.42%) - LeetHub

---
+++ 621-task-scheduler/621-task-scheduler.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int leastInterval(char[] tasks, int n) {
+        if(n == 0) return tasks.length;
+        
+        int[] arr = new int[26];
+        Arrays.fill(arr, 0);
+        
+        for(int i = 0; i < tasks.length; i++){
+            arr[tasks[i] - 'A']++;
+        }
+        Arrays.sort(arr);
+        
+        int chunks = arr[25] - 1;
+        int idle = chunks * n;
+        for(int i = 24; i >= 0; i--){
+            idle -= Math.min(chunks, arr[i]);
+        }
+        
+        if(idle < 0) return tasks.length;
+        return tasks.length + idle;
+    }
+    // TC & SC : O(n)
+}
