3e01f169f91825cd41ff16bea49f71c5902be44e
Roshan-Pawar <90392961+Roshan-Pawar@users.noreply.github.com>
2022-09-06 13:58:54 +0530

    Time: 3 ms (81.99%), Space: 42.7 MB (75.99%) - LeetHub

--- 32-longest-valid-parentheses/32-longest-valid-parentheses.java
+++ 32-longest-valid-parentheses/32-longest-valid-parentheses.java
@@ -1,21 +1,39 @@
 class Solution {
     public int longestValidParentheses(String s) {
-        Stack<Integer> stack = new Stack<>();
-        stack.push(-1);
+        int open = 0, close = 0;
         int max = 0;
+        
+        // 0 --> N
         for(int i = 0; i < s.length(); i++){
             char ch = s.charAt(i);
-            if(ch == '(') stack.push(i);
-            else{
-                stack.pop();
-                if(stack.empty()) stack.push(i);
-                else{
-                    int len = i - stack.peek();
-                    max = Math.max(max, len);
-                } 
+            if(ch == '(') open++;
+            else close++;
+            
+            if(open == close){
+                int len = open + close;
+                max = Math.max(max, len);   
+            }
+            else if(close > open){
+                open = close = 0;
+            }
+        }
+        
+        // N --> 0
+        open = close = 0;
+        for(int i = s.length() - 1; i >= 0; i--){
+             char ch = s.charAt(i);
+            if(ch == '(') open++;
+            else close++;
+            
+            if(open == close){
+                int len = open + close;
+                max = Math.max(max, len);   
+            }
+            else if(close < open){
+                open = close = 0;
             }
         }
         return max;
     }
-    // TC & SC : O(n)
+    // TC : O(n) & SC : O(1)
 }
