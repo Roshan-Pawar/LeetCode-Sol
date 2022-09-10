5310c2d63a02f65af519aa9a4b2c459a890fd6e3
Roshan-Pawar <90392961+Roshan-Pawar@users.noreply.github.com>
2022-09-10 14:07:44 +0530

    Time: 1 ms (98.37%), Space: 42.6 MB (26.08%) - LeetHub

--- 771-jewels-and-stones/771-jewels-and-stones.java
+++ 771-jewels-and-stones/771-jewels-and-stones.java
@@ -1,16 +1,13 @@
 class Solution {
     public int numJewelsInStones(String jewels, String stones) {
-        char[] jewls = jewels.toCharArray();
-        char[] stone = stones.toCharArray();
         int ans = 0;
-        
-        for(int i = 0; i < jewls.length; i++){
-            for(int j = 0; j <stone.length; j++){
-                if(jewls[i] == stone[j])
+        for(int i = 0; i < jewels.length(); i++){
+            for(int j = 0; j < stones.length(); j++){
+                if(jewels.charAt(i) == stones.charAt(j))
                     ans++;
             }
         }
         return ans;
     }
-    // TC : O(n^2) & SC : O(n)
+    // TC : O(n^2)
 }
