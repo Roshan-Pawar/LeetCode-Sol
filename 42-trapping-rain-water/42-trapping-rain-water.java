bf0bf5ca14640e4e0116de05dee722264cda181c
Roshan-Pawar <90392961+Roshan-Pawar@users.noreply.github.com>
2022-09-18 13:20:45 +0530

    Time: 3 ms (28.65%), Space: 49.6 MB (14.74%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.java
+++ 42-trapping-rain-water/42-trapping-rain-water.java
@@ -4,24 +4,24 @@ class Solution {
         int[] maxLeft = new int[n];
         int[] maxRight = new int[n];
         
-        // Find left Maximum of every element in array
+        // Max left
         maxLeft[0] = height[0];
         for(int i = 1; i < n; i++){
             maxLeft[i] = Math.max(maxLeft[i - 1], height[i]);
         }
         
-        // Find right maximum of evenry element in array from right
+        // Max Right
         maxRight[n - 1] = height[n - 1];
-        for(int i = n - 2; i >=0; i--){
+        for(int i = n - 2; i >= 0; i--){
             maxRight[i] = Math.max(maxRight[i + 1], height[i]);
         }
         
-        int sum = 0;
-        // Calculating sum by Minimum at every[i] - height[i]
+        // Calculating depth to trap rainwater
+        int ans = 0;
         for(int i = 0; i < n; i++){
-            sum += Math.min(maxLeft[i], maxRight[i]) - height[i];
+            ans += Math.min(maxRight[i], maxLeft[i]) - height[i];
         }
-        return sum;
+        return ans;
     }
-    // O(N) time and space
+    // TC & SC : O(n)
 }
