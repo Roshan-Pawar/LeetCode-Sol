fac0e47f40d5a3d14552a9621405188d60a844ea
Roshan-Pawar <90392961+Roshan-Pawar@users.noreply.github.com>
2022-10-23 20:09:03 +0530

    Time: 6 ms (53.46%), Space: 54.6 MB (51.12%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.java
+++ 0645-set-mismatch/0645-set-mismatch.java
@@ -1,20 +1,22 @@
 class Solution {
     public int[] findErrorNums(int[] nums) {
-        int[] arr = new int[nums.length];
         int[] ans = new int[2];
         
         for(int num : nums){
-            arr[num - 1]++;
+            if(nums[Math.abs(num) - 1] < 0){
+                ans[0] = Math.abs(num);
+            } else {
+                nums[Math.abs(num) - 1] *= -1;
+            }
         }
         
         for(int i = 0; i < nums.length; i++){
-            if(arr[i] == 2){
-                ans[0] = i + 1;
-            }
-            if(arr[i] == 0){
+            if(nums[i] > 0){
                 ans[1] = i + 1;
+                break;
             }
         }
         return ans;
     }
+    // TC : O(n)
 }
