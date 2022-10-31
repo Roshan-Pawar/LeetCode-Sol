e99a0c9109b6f6b5272f3c3a769db02f46ea13ab
Roshan-Pawar <90392961+Roshan-Pawar@users.noreply.github.com>
2022-10-31 11:05:23 +0530

    Time: 1 ms (93.83%), Space: 45.7 MB (54.67%) - LeetHub

---
+++ 0766-toeplitz-matrix/0766-toeplitz-matrix.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isToeplitzMatrix(int[][] matrix) {
+        for(int row = 1; row < matrix.length; row++){
+            for(int col = 1; col < matrix[0].length; col++){
+                if(matrix[row - 1][col - 1] != matrix[row][col])
+                    return false;
+            }
+        }
+        return true;
+    }
+    // check for upper left value is equal or not
+    // TC : O(m * n)
+}
